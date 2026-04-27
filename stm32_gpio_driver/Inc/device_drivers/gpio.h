ca236584aac8052d1d24539431bc4cb3f9ec1d61
jaiwantD <jaiwantd.act2023@citchennai.net>
2026-04-27 21:53:51 +0530

    GPIO-Driver: Header

--- stm32_gpio_driver/Inc/device_drivers/gpio.h
+++ stm32_gpio_driver/Inc/device_drivers/gpio.h
@@ -8,7 +8,21 @@
 #ifndef DEVICE_DRIVERS_GPIO_H_
 #define DEVICE_DRIVERS_GPIO_H_
 
-void GPIO_Init(Port, pin, mode , o);
+typedef struct{
+	GPIO_TypeDef *port;
+	uint8_t pin;
+	uint8_t mode;
+
+    uint8_t otype;
+    uint8_t speed;
+    uint8_t pupd;
+    uint8_t alt;
+}GPIO_Config;
+
+#define input_mode     0
+#define output_mode    1
+#define altf_mode     2
+#define analog_mode    3
 
 
 
