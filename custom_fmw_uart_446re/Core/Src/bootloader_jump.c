7b2d89bf60387df4f9caa3432ec9abe753de08f6
jaiwantD <jaiwantd.act2023@citchennai.net>
2026-05-03 22:08:28 +0530

    UART no bootpin: Minor Fix

--- custom_fmw_uart_446re/Core/Src/bootloader_jump.c
+++ custom_fmw_uart_446re/Core/Src/bootloader_jump.c
@@ -8,16 +8,12 @@
 #include "stm32f4xx_hal.h"
 
 #define SYS_MEM_ADDR 0x1FFF0000U   // System Memory base for STM32F446
-
 void JumpToBootloader(void)
 {
     void (*SysMemBootJump)(void);
     uint32_t addr = SYS_MEM_ADDR;
-
     /* 1. Disable interrupts */
     __disable_irq();
-
-    /* 2. Deinitialize peripherals & clocks */
     HAL_DeInit();
     HAL_RCC_DeInit();
 
@@ -32,19 +28,12 @@ void JumpToBootloader(void)
         NVIC->ICPR[i] = 0xFFFFFFFF;
     }
 
-    /* 5. (If used) deinit USB / UART explicitly */
-    // HAL_UART_DeInit(&huart1);
-    // __HAL_RCC_USB_OTG_FS_CLK_DISABLE();
-
-    /* 6. Small delay (let lines settle) */
     for (volatile uint32_t i = 0; i < 100000; i++);
-
     /* 7. Get reset handler from system memory */
     SysMemBootJump = (void (*)(void)) (*((uint32_t *)(addr + 4U)));
 
     /* 8. Set MSP to system memory value */
     __set_MSP(*(uint32_t *)addr);
-
     /* 9. Jump to system bootloader */
     SysMemBootJump();
 
